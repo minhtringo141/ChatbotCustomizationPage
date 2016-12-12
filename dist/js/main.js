61280d023f87d202e70ba85f5db5d36f0e9f7a29
Minh Trí <minhtringo141@gmail.com>
2016-12-12 16:22:39 +0700

    config gallery input form

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,14 +7,14 @@ $("#btn_add").click(function () {
 });
 
 
-jQuery.each(jQuery('textarea[data-autoresize]'), function() {
-    var offset = this.offsetHeight - this.clientHeight;
+// jQuery.each(jQuery('textarea[data-autoresize]'), function() {
+//     var offset = this.offsetHeight - this.clientHeight;
  
-    var resizeTextarea = function(el) {
-        jQuery(el).css('height', 'auto').css('height', el.scrollHeight + offset);
-    };
-    jQuery(this).on('keyup input', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
-})
+//     var resizeTextarea = function(el) {
+//         jQuery(el).css('height', 'auto').css('height', el.scrollHeight + offset);
+//     };
+//     jQuery(this).on('keyup input', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
+// })
 
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
